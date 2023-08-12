e524a1328a5c93bce25b0f3a1ba9b425e595e51b
arsenic-47 <imyardan@gmail.com>
2023-08-12 10:21:17 +0530

    done

--- index.js
+++ index.js
@@ -20,6 +20,9 @@ client.on("messageCreate", async message => {
   if(message.content.toLowerCase() === "gay") {
     message.channel.send("why are you gay")
   }
+  if(message.content.toLowerCase() === "im not gay") {
+    message.channel.send("you are gay")
+  }
   if(message.content.toLowerCase() === "ba bitches") {
     message.channel.send("ba ba")
   }
